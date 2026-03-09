c98f4a9f82796e18854a49137b3551317807de1e
Hardik Dagar <hardikdagar1803@gmail.com>
2026-03-09 09:12:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = "Hardik";
+console.log(typeof name);
+let age = 22;
+console.log(typeof age);
+let isStudent = true;
+console.log(typeof isStudent);
+let score = null;
+console.log(typeof score);
+let person = {name: "Hardik", age: 22};
+console.log(person);
+
+
