a59fac9a3391f8f3eb9877905378440a737dd3b5
Hardik Dagar <hardikdagar1803@gmail.com>
2026-03-09 09:11:31 +0530

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,26 @@
+let animal = 4;
+switch(animal){
+    case 1:
+        console.log("Deer");
+        break;
+    case 2:
+        console.log("Tiger");
+        break;
+    case 3:
+        console.log("Frog");
+        break;
+    case 4:
+        console.log("Giraffe");
+        break;
+    case 5:
+        console.log("Elephant");
+        break;
+    case 6:
+        console.log("Monkey");
+        break;
+    case 7:
+        console.log("Lion");
+        break;
+    default:
+        console.log("Not in the zoo");
+}
