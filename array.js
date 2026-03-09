5d398c510cd6cf5596a292a636a2ae54c839dc3b
Hardik Dagar <hardikdagar1803@gmail.com>
2026-03-09 09:42:30 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,9 @@
+let marks = [85, 90, 78, 92, 88];
+console.log(marks[0]);
+console.log(marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+console.log(marks[4]);
+
+console.log(marks.length);
+console.log(typeof marks);
