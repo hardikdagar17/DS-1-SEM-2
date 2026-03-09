8caf0209a9611687a6912d0d95a83b5a23a27869
Hardik Dagar <hardikdagar1803@gmail.com>
2026-03-09 09:12:46 +0530

    Add files via upload

---
+++ relational.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = '10';
+console.log(a==b);     //comparion between values of variables
+console.log(a!=b);
+console.log(a===b);    //comparison with type of variables
+let name = "Hardik";
+let naam = "Dagar";
+console.log(name + " " +naam);
