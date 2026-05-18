93d0a670fc4ab24daaf1cd949ba3bf2dee5c31db
Hardik Dagar <hardikdagar1803@gmail.com>
2026-05-18 09:08:58 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(++a);
+// console.log(a++);
+// console.log(--b);
+// console.log(b--);
+// console.log(a);
+// console.log(b);
+// console.log(a+=3);
+// console.log(a-=3);
+// console.log(a*=3);
+// console.log(a/=3);
+// console.log(a%=3);
